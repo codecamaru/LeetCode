bfcedc15922185a531b6bc6fa341818e191fefb7
Carolina Marugan Rubio <54485798+codecamaru@users.noreply.github.com>
2022-06-12 18:33:19 +0200

    Update Maximum_Depth_of_Binary_Tree.cpp

--- Maximum_Depth_of_Binary_Tree.cpp
+++ Maximum_Depth_of_Binary_Tree.cpp
@@ -1,3 +1,6 @@
+/* Leetcode: Given the root of a binary tree, return its maximum depth. 
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node. */
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
